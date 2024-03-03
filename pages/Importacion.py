dd021a5e252ecdb0c989bf71ed10ea2824d311f3
plealm <plealm@unal.edu.co>
2024-03-03 07:22:53 -0500

    primeras dos hojas

---
+++ pages/Importacion.py
@@ -0,0 +1,101 @@
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import yfinance as yf
+import numpy as np
+import datetime as dt
+import warnings
+import matplotlib.pyplot as plt
+
+ticker_name = 'BC'
+
+data = yf.download(ticker_name, start='2000-01-01', end='2020-01-01')
+df = pd.DataFrame()
+df["Date"] = pd.to_datetime(data.index).date
+df["Close"] = data["Close"].values
+
+fig = go.Figure()
+# Suponiendo que ya tienes el DataFrame df
+
+fig.add_trace(go.Scatter(
+    x=df["Date"],
+    y=df["Close"],
+    name="NeuralProphet Forescasting",
+    line=dict(color='blue', width=2)
+))
+fig.update_xaxes(type='category')  # Para que las fechas se muestren correctamente
+
+# Centrar el título
+fig.update_layout(title_text='Precio de acciones Bancolombia', title_x=0.5)
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1d",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=7,
+                     label="1w",
+                     step="day",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=3,
+                     label="3m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type="date"
+    ),
+    yaxis=dict(
+        autorange=True,
+        type="linear"
+    )
+)
+
+fig.update_layout(
+    xaxis_title="Date",
+    yaxis_title="Price",
+    legend=dict(
+        orientation="v",
+        yanchor="top",
+        y=1,
+        xanchor="left",
+        x=-.35
+    )
+)
+
+dash.register_page(__name__)
+
+df = px.data.tips()
+
+layout = html.Div(
+    [
+        dcc.Markdown('''
+                     Los datos utilizados en este análisis fueron 
+                     adquiridos mediante la herramienta **yfinance**, con el fin de obtener 
+                     el precio de cierre de las acciones de Bancolombia.
+                     ''', style={'text-align': 'center', 'margin-bottom': '20px', 'max-width': '800px', 'margin-left': 'auto', 'margin-right': 'auto'}),
+        dcc.Graph(figure=fig)
+    ]
+)
+
---
+++ pages/intro.py
@@ -0,0 +1,32 @@
+import dash
+from dash import dcc, html
+import plotly.express as px
+
+dash.register_page(__name__, path='/')
+
+layout = html.Div([
+    dcc.Markdown('''
+        ## Introducción       
+    '''
+    , style={'text-align': 'center', 'margin-bottom': '60px'}
+    ),
+
+    dcc.Markdown('''
+        Este análisis se centra en el **precio de cierre** vs **tiempo** de las acciones de Bancolombia durante el periodo comprendido entre el 2020-01-01 y el 2020-01-01. La elección de este periodo se fundamenta en evitar la inclusión de datos durante o después de la pandemia. Dada la naturaleza de la serie temporal de acciones, se anticipa un comportamiento de tipo caminata aleatoria. Además, es importante destacar que esta serie temporal presenta datos no uniformemente distribuidos en el tiempo, ya que los sábados, domingos y días festivos no hay actividad en la bolsa.
+
+    '''
+    ,  style={'text-align': 'center', 'margin-bottom': '70px', 'max-width': '800px', 'margin-left': 'auto', 'margin-right': 'auto'}
+    ),
+
+    dcc.Markdown('''
+        **Autores:** Pedro Leal, Luis Mantilla, Bryam Bustos.
+    '''
+    , style={'text-align': 'center', 'margin-bottom': '20px'}
+    ),
+])
+
+
+
+
+
+
